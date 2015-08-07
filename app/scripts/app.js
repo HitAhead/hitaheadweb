502a274ae596190c64a8aae277b864d2d775d1ca
Sriramkumar Jothimani <sriramjothimani@gmail.com>
2015-08-07 23:13:27 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testappApp
+ * @description
+ * # testappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hitaheadweb', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ]);
